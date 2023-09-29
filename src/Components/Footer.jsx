4bf9090aad9f51775f876ddd511423e8ee59376b
Sajith-Kumar-S-B <sajithkumarsb1@gmail.com>
2023-09-29 12:33:30 +0530

    v1

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Footer() {
+  return (
+   
+      <div style={{width:'100%',height:'300px'}} className='d-flex justify-content-center align-items-center flex-column'>
+         <div className='footer-content d-flex justify-content-evenly align-items-center w-100 flex-wrap'>
+               <div style={{width:'400px'}}  className='website'>
+              
+                   <h4><i class="fa-solid fa-play fa-flip"></i>{' '}Media Player</h4>
+                   <h6>Designed and built with all the love in the world by the Bootstrap team with the help of our contributors.</h6>
+                  <h6>Code licensed MIT, docs CC BY 3.0.</h6>
+                   <p>Currently v1.0.0</p>
+               </div>
+              
+         
+           <div className='links d-flex flex-column' >
+          <h5> Links</h5>
+      <Link to={'/home'} style={{color:'black',textDecoration:'none'}}>Landing Page</Link>
+      <Link to={'/'} style={{color:'black',textDecoration:'none'}}>Home</Link>
+      <Link to={'/watch-history'} style={{color:'black',textDecoration:'none'}}>Watch History</Link>
+
+      
+           </div>
+           <div className='guides d-flex flex-column'>
+            <h5>Guides</h5>
+            <Link to={'https://react-bootstrap.netlify.app/'} style={{color:'black',textDecoration:'none'}}>React</Link>
+      <Link to={'https://getbootstrap.com/'} style={{color:'black',textDecoration:'none'}}>Bootstrap</Link>
+      <Link to={'https://fontawesome.com/'} style={{color:'black',textDecoration:'none'}}>Fontawesome</Link>
+
+    
+            </div>
+            <div className='contact'>
+              <h4>Contact Us</h4>
+              <div className='d-flex justify-content-center align-items-center gap-2 '>
+                <input className='form-control rounded' type="text" placeholder='Enter your Email' />
+              <button className='bg-primary' style={{borderRadius:'10px',padding:'5px 20px',color:"white"}}>Subscribe</button></div>
+            </div>
+          </div>
+          <p>Media Player Copyright@2023 Made with React</p>
+      </div>
+      
+  )
+}
+
+export default Footer
---
+++ src/Pages/Landingpage.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import {Row,Col} from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+
+function Landingpage() {
+  return (
+    <>
+    <Row className='mt-5 mb-5 justify-content-center  align-items-center'>
+      <Col lg={4}>
+        <h3 className='mb-3'>Welcome to <span className='text-warning'>Media Player</span></h3>
+        <p style={{textAlign:'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga inventore sapiente quas odio aperiam illo porro esse natus omnis rerum cumque asperiores quam, deserunt nostrum excepturi perferendis consectetur. Id, maiores!</p>
+        <button className='btn btn-primary mt-5 fw-bolder'>Get Started</button>
+      </Col>
+
+      <Col className='ps-2' lg={6}>
+
+        <img className='img-fluid ms-5 ps-5 w-100' src="https://i.pinimg.com/originals/cc/0e/d3/cc0ed34dd09adf414a19a69b460804f4.gif" alt="" />
+      </Col>
+
+    </Row>
+    <div className="container mb-5 mt-5 d-flex flex-column justify-content-between align-items-center">
+      <h3>Features</h3>
+   <div className='cards d-flex mt-5 justify-content-between align-items-center w-100'>
+      <Card className='p-3' style={{ width: '22rem',border:'none' }}>
+        <Card.Img style={{height:'300px',width:'300px'}} variant="top" src="https://i.gifer.com/Z23N.gif" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className='p-3' style={{ width: '22rem' ,border:'none'}}>
+        <Card.Img style={{height:'300px',width:'300px'}} variant="top" src="https://media.tenor.com/iUvxx_10mRQAAAAi/abiera-music.gif" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className='p-3' style={{ width: '22rem',border:'none' }}>
+        <Card.Img style={{height:'300px',width:'320px'}} variant="top" src="https://i.pinimg.com/originals/01/4e/db/014edb1a17e81b769f26637cdb69eddf.gif" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+   </div>
+    </div>
+    <div className='conatiner mb-5 mt-5 d-flex justify-content-between p-5 w-100 align-items-center'>
+       <div className='content'>
+        <h3 className='text-warning mb-5'>
+          Simple,Fast and Powerful
+        </h3>
+        <h6 className='mt-4'> <span className='fs-5 fw-bolder'>Play Everything </span>: Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt et, dolor temporibus rem error commodi, odit eligendi alias recusandae nobis dicta itaque officiis perferendis quae totam rerum maxime? Assumenda, voluptates?</h6>
+        <h6 className='mt-4'> <span className='fs-5 fw-bolder'>Categorise Videos </span> : Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt et, dolor temporibus rem error commodi, odit eligendi alias recusandae nobis dicta itaque officiis perferendis quae totam rerum maxime? Assumenda, voluptates?</h6>
+       </div>
+       <div className='video ms-5'>
+       
+        <iframe width="689" height="387" src="https://www.youtube.com/embed/IqwIOlhfCak" title="LEO - Badass Lyric | Thalapathy Vijay | Lokesh Kanagaraj | Anirudh Ravichander" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        
+       </div>
+    </div>
+    </>
+  )
+}
+
+export default Landingpage
